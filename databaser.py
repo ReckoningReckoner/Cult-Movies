ef15792294537118b4e4606412687c2981bcb4d8
Viraj Bangari <Viraj3f@gmail.com>
2016-02-20 17:44:25 -0500

    Backend SQL stuffssss

--- databaser.py
+++ databaser.py
@@ -1,19 +1,51 @@
 import metacritic
+import shlex
+import mysql.connector
+import sys
+from datetime import date, datetime, timedelta
+
 
 def is_cult(crit, user):
     return crit <= 50 and user >= 60
+    
+def get_cult(title):
+    result = metacritic.Metacritic.search(title)
+    for r in result:
+        if r.metascore and r.user_score:
+            metascore = int(r.metascore)
+            user_score = int(float(r.user_score))*10
+            release_date = datetime.strptime(r.release_date, '%B %d, %Y').strftime("%Y-%m-%d")
+            if is_cult(metascore, user_score):
+                yield r.id, r.title, metascore, user_score, release_date, r.link
+                
+db = mysql.connector.connect(user='sql5107655', 
+                            password='izqAeyN6Yu', 
+                            host='sql5.freemysqlhosting.net', 
+                            database='sql5107655')
+cursor = db.cursor()
 
-
-result = metacritic.Metacritic.search("Inside Out")
-for r in result:
-    if r.metascore and r.user_score:
-        
-        metascore = int(r.metascore)
-        user_score = int(float(r.user_score))*10
-        
-        if is_cult(metascore, user_score):
-            print(r.title)
-            print(metascore)
-            print(user_score)
-            print(r.release_date)
-            print(r.link)
+i = 0
+with open("movies.list.txt") as f:
+    for line in f:
+        try:
+            line = f.readline()
+            line = shlex.split(line)
+            print(i)
+            for id, title, metascore, user_score, release_date, url in get_cult(line[0]):
+                try:
+                    print(id, title, metascore, user_score, url, release_date)
+                    args = (id, title, metascore, user_score, url, release_date)
+                    command = "INSERT INTO `movie` VALUES(%s, %s, %s, %s, %s, %s) "
+                    cursor.execute(command, args)
+                except:
+                    print(sys.exc_info()[0])
+                    continue
+            i += 1
+            
+        except:
+            print(sys.exc_info()[0])
+            continue
+            
+db.commit()
+cursor.close()
+db.close()            
